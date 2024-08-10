530b45885b24fef40e50b569379b6243c29dbaf9
hariprasad-11ai <hariprasadnarla4@gmail.com>
2024-08-09 20:18:22 -0500

    initial commit

---
+++ src/main/java/com/java/springbootfirstapp/SpringbootfirstappApplication.java
@@ -0,0 +1,13 @@
+package com.java.springbootfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfirstappApplication.class, args);
+		//System.out.println("Hi this is Hari");
+	}
+}
---
+++ src/main/java/com/java/springbootfirstapp/controller/JavaController.java
@@ -0,0 +1,15 @@
+package com.java.springbootfirstapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hi")
+public class JavaController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+}
